5b9d3cdc567d87e8300b89361e9d84a79cf10cc1
Dylan Weed <dylanweed@gmail.com>
2026-07-01 11:39:01 -0700

    Scaffold Vite + React + TypeScript app

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="app__main">
+        <h1>Health Habits</h1>
+      </div>
+    </div>
+  );
+}
